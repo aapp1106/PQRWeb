894de08639ed51d8b389b6cfb5777295e672c01f
Alberto Perez <aapp1106@gmail.com>
2021-10-20 00:30:42 -0500

    feat: integrate_material_angular
    the following libraries were added:
    angular material -> css framework
    flexboxgrid -> grid in DOM

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'pqrs', loadChildren: () => import('./modules/pqrs/pqrs.module').then(m => m.PqrsModule) }];
+const routes: Routes = [
+  { path: 'pqrs', loadChildren: () => import('./modules/pqrs/pqrs.module').then(m => m.PqrsModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
